cfaa737596b92dfad8c42bb897e3b426aaa37be9
tonynater <tong.t.wang@gmail.com>
2014-03-08 23:11:26 -0800

    deleted duplicate
    deleted a duplicate version of getgraph.py that was accidentally left
    outside of the scripts folder.

--- apache2/htdocs/getgraph.py
+++
@@ -1,6 +0,0 @@
-import smtplib
-
-def nextbutton(req, token):
-	req.content_type = 'text/html'
-	return "test"
-#return '<form><button type="submit" formaction="button.py/nextbutton" formmethod="post" formtarget="_parent">You will regret this.</button></form>'
