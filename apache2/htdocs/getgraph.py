f1f37cc30284776bb3757ad24bf18784579e4fd1
tonynater <tong.t.wang@gmail.com>
2014-03-08 23:09:13 -0800

    reorganizing scripts and script query
    Reorganized the scripts (and renamed them) and put them in a new folder
    called scripts. Also added jquery code to interface with the scripts.

--- apache2/htdocs/getgraph.py
+++ apache2/htdocs/getgraph.py
@@ -1,6 +1,6 @@
 import smtplib
 
-def nextbutton(req):
+def nextbutton(req, token):
 	req.content_type = 'text/html'
-	return "Hello, this is a test."
+	return "test"
 #return '<form><button type="submit" formaction="button.py/nextbutton" formmethod="post" formtarget="_parent">You will regret this.</button></form>'
