a43595f9f365788d1a389f58c4614a7add0483fb
tonynater <tong.t.wang@gmail.com>
2014-03-09 00:28:54 -0800

    Added pictures, added notification on leave
    Added picture folder (that isn’t working right now), also added a
    dialog that pops up when leaving (since generating graph takes a long
    time.)

--- apache2/htdocs/scripts/getgraph.py
+++
@@ -1,6 +0,0 @@
-import smtplib
-
-def nextbutton(req, access):
-	req.content_type = 'text/html'
-	return "Hello, this is a test."
-#return '<form><button type="submit" formaction="button.py/nextbutton" formmethod="post" formtarget="_parent">You will regret this.</button></form>'
---
+++ apache2/htdocs/scripts/graph.py
@@ -0,0 +1,5 @@
+import smtplib
+
+def getgraph(req):
+	req.content_type = 'text/html'
+	return "random stuff not legit"
